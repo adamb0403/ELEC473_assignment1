653e78c79935f2563f18b3ddabb24fb76b6db5d2
Adam Butt <adamskibutt@gmail.com>
2023-11-17 23:40:03 +0000

    Single pulser verilog and simulation

--- tx_single_pulser/tx_single_pulser.v
+++ tx_single_pulser/tx_single_pulser.v
@@ -1,17 +1,34 @@
 module tx_single_pulser (input clk,
                          input reset,
                          input pulser_in,
-                         output pulser_out);
+                         output reg pulser_out);
 
-    parameter wait_h = 1'b1;
-    parameter wait_l = 1'b0;
+    reg current_state;
+    parameter wait_h = 1'b0;
+    parameter wait_l = 1'b1;
+
+    initial
+        current_state <= wait_h;
 
     always @(posedge clk) begin
         if (!reset) begin
+            current_state <= wait_h;
             pulser_out <= 1'b0;
         end
         else begin
-
+            pulser_out <= 1'b0;
+            case (current_state)
+                wait_h: begin
+                    if (!pulser_in) begin
+                        pulser_out <= 1'b1;
+                        current_state <= wait_l;
+                    end
+                end
+                wait_l: begin
+                    if (pulser_in)
+                        current_state <= wait_h;
+                end
+            endcase
         end
     end
 endmodule
